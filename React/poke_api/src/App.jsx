ea5d2610a59d018a5c7a16a434c9114f795aa67d
Dan Maisano <danmaisano@gmail.com>
2023-06-24 12:13:11 -0400

    pokeapi

---
+++ React/poke_api/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ShowPoke from './components/showPoke'
+
+function App() {
+
+  
+  return (
+    <>
+      <ShowPoke/>
+    </>
+  )
+}
+
+export default App
---
+++ React/poke_api/src/components/showPoke.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import axios from "axios"
+
+const ShowPoke = () => {
+    const [pokeList, setPokeList] = useState([])
+    let [count, setCount] = useState(1)
+    useEffect(() => {
+        if (count < 808) {
+            axios.get("https://pokeapi.co/api/v2/pokemon/" + count).then(response => {
+            setPokeList([...pokeList, response.data.name])
+            setCount(count += 1)
+            })
+        }
+        else{
+            return
+        }
+    }, [count]);
+
+    return(
+        <div>
+            {pokeList.map((poke,idx)=>(
+                <p key={idx}>{poke}</p> 
+                    
+            ))}
+        </div>
+    )
+}
+
+export default ShowPoke;
